23489856e1c79d69eacc29dc4eed67e2ebebe21e
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-03-08 21:06:38 +0200

    teht 6-19 styling for notifications

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -4,14 +4,20 @@ const Notification = ({ notificationText }) => {
   const notification = notificationText
   const style = {
     backgroundColor: '#dbffc9',
+    border: 'solid',
+    borderColor: '#9bb290',
     padding: 10,
-    borderWidth: 1
+    borderWidth: 1,
+    marginTop: 5
   }
 
   const emptyStyle = {
     backgroundColor: '#ffffff',
+    border: 'solid',
+    borderColor: '#000000', //'#9bb290',
     padding: 10,
-    borderWidth: 1
+    borderWidth: 1,
+    marginTop: 5
   }
 
   const notificationElement = () => (
