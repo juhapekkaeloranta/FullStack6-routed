954a5e0e05f1a5690133cd5d5c81c629416752c4
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-03-08 16:55:52 +0200

    refactor components to files

--- src/App.js
+++ src/App.js
@@ -1,93 +1,9 @@
 import React from 'react'
-
-const Menu = () => (
-  <div>    
-    <a href='#'>anecdotes</a>&nbsp;
-    <a href='#'>create new</a>&nbsp;
-    <a href='#'>about</a>&nbsp;
-  </div>
-)
-
-const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
-    </ul>  
-  </div>
-)
-
-const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
-
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
-)
-
-const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
-
-    See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code. 
-  </div>
-)
-
-class CreateNew extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      content: '',
-      author: '',
-      info: ''
-    }
-  }
-
-  handleChange = (e) => {
-    console.log(e.target.name, e.target.value)
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.addNew({
-      content: this.state.content,
-      author: this.state.author,
-      info: this.state.info,
-      votes: 0
-    })
-  }
-
-  render() {
-    return(
-      <div>
-        <h2>create a new anecdote</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            content 
-            <input name='content' value={this.state.content} onChange={this.handleChange} />
-          </div>
-          <div>
-            author
-            <input name='author' value={this.state.author} onChange={this.handleChange} />
-          </div>
-          <div>
-            url for more info
-            <input name='info' value={this.state.info} onChange={this.handleChange} />
-          </div> 
-          <button>create</button>
-        </form>
-      </div>  
-    )
-
-  }
-}
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import AnecdoteList from './components/AnecdoteList'
+import About from './components/About'
+import Footer from './components/Footer'
+import CreateNew from './components/CreateNew'
 
 class App extends React.Component {
   constructor() {
@@ -111,7 +27,7 @@ class App extends React.Component {
         }
       ],
       notification: ''
-    } 
+    }
   }
 
   addNew = (anecdote) => {
@@ -139,10 +55,18 @@ class App extends React.Component {
     return (
       <div>
         <h1>Software anecdotes</h1>
-          <Menu />
-          <AnecdoteList anecdotes={this.state.anecdotes} />
-          <About />      
-          <CreateNew addNew={this.addNew}/>
+        <Router>
+          <div>
+            <div>
+              <Link to="/">anecdotes</Link> &nbsp;
+              <Link to="/new">create new</Link> &nbsp;
+              <Link to="/about">about</Link>
+            </div>
+            <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
+            <Route path="/new" render={() => <CreateNew />} />
+            <Route path="/about" render={() => <About />} />
+          </div>
+        </Router>
         <Footer />
       </div>
     );
---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const AnecdoteList = ({ anecdotes }) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <ul>
+      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+    </ul>
+  </div>
+)
+
+export default AnecdoteList
